a88eb5b176c089446b173578acd350be5762968e
Eduardo Rett Guirelli <eduardorett@hotmail.com>
2021-12-15 17:40:02 -0300

    Adicionado Login via firebase

--- app/src/main/java/br/com/alfa11/devedu/treinos/views/LoginActivity.kt
+++ app/src/main/java/br/com/alfa11/devedu/treinos/views/LoginActivity.kt
@@ -2,25 +2,97 @@ package br.com.alfa11.devedu.treinos.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alfa11.devedu.treinos.R
-import br.com.alfa11.devedu.treinos.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity: AppCompatActivity() {
+
+    private var auth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance();
+
+
         button_login.setOnClickListener {
-            startActivity(Intent(this, MainAcitivity::class.java))
+
+            val email = edit_email.text.toString()
+            val senha = edit_password.text.toString()
+
+            signIn(email, senha)
+
         }
 
         text_register.setOnClickListener{
             startActivity(Intent(this, CadastroUsuarioActivity::class.java))
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth!!.currentUser
+        //updateUI(currentUser)
+    }
+
+    private fun createAccount(email: String, password: String) {
+
+        auth!!.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth!!.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun signIn(email: String, password: String) {
+
+        auth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth!!.currentUser
+                    updateUI(user)
+                    startActivity(Intent(this, MainAcitivity::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+    private fun reload() {
+
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
 
